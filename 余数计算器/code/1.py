6977a5191267829e45460c480a004062c74520cc
dzytool <88652490+dzytool@users.noreply.github.com>
2021-11-06 14:42:05 +0800

    Delete 余数计算器 directory

--- 余数计算器/code/1.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:gbk -*-
-import os
-while True:
-    x1 = input("被除数：")
-    x2 = input("除数：")
-    print "结果：" + str(x1 / x2)
-    print "余数：" + str(x1 % x2)
-    os.system("pause&cls")
